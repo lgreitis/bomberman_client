342905a74d64f3472488c9d33d5fa741a42342e0
lgreitis <lukas.greicius@ktu.edu>
2022-11-06 18:53:48 +0200

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
-      jsxImportSource: "@emotion/react",
+      jsxImportSource: '@emotion/react',
       babel: {
-        plugins: ["@emotion/babel-plugin"],
+        plugins: ['@emotion/babel-plugin'],
       },
     }),
   ],
   server: {
     cors: {
-      allowedHeaders: "*",
+      allowedHeaders: '*',
     },
   },
   esbuild: {
-    logOverride: { "this-is-undefined-in-esm": "silent" },
+    logOverride: { 'this-is-undefined-in-esm': 'silent' },
   },
 });
